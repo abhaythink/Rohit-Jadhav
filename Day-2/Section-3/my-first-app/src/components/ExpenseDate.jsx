271d4faa26c0cf88f66e1d8534c911d19c5b31ee
Rohit Jadhav <rohit.jadhav@thinkitive.com>
2025-12-16 20:00:07 +0530

    initial commit

---
+++ Day-2/Section-3/my-first-app/src/components/ExpenseDate.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ExpenseDate.css'
+import Card from './Card';
+const ExpenseDate = ({date}) => {
+    let month = date.toLocaleString('en-US',{month:'long'});
+    let day= date.toLocaleString('en-US',{day:'2-digit'});
+    let year= date.getFullYear();
+   
+  return (
+    <>
+     <Card className="expense-date">
+                <div className="expense-date__day">{day}</div>
+                <div className="expense-date__month">{month}</div>
+                <div className="expense-date__year">{year}</div>
+            </Card>
+    </>
+  )
+}
+
+export default ExpenseDate
---
+++ Day-2/Section-5/expense-tracker/src/components/Chart/Chart.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Chart.css'
+import ChartBar from './ChartBar'
+const Chart = (props) => {
+    const dataPointValue = props.dataPoint.map((e)=>e.value)
+    const totalMaximum = Math.max(...dataPointValue);
+    // console.log(totalMaximum);
+  return (
+    <div className='chart'>
+        {props.dataPoint.map(dataPoint=>
+            <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label}/>
+        )}
+    </div>
+  )
+}
+
+export default Chart
